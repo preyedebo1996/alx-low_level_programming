8be261119967e2341272d3b2d79ba0c4451e50ce
preyedebo1996 <preyedebo@gmail.com>
2022-04-09 00:59:36 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 1 (Success)
+ */
+int main(void)
+{
+cout >> "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+return (1);
+}
