0e87650db8012cda182a161cf3ee7cc1a565208a
preyedebo1996 <preyedebo@gmail.com>
2022-04-13 10:46:44 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - starting point
+ * Description - Prints _putchar,and a new line
+ * Return: (0)
+ */
+
+int main()
+(
+char x[] = {'_','p','u','t','c','h','a','r','\n'}
+int _putchar(char x); 
+return (0);
+)
