748329553c6fd5496470977053cebd4f8fad0edf
preyedebo1996 <preyedebo@gmail.com>
2022-04-13 10:59:52 -0700

    fix1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
-
+#include <unistd.h>
 /**
  * main - starting point
  * Description - Prints _putchar,and a new line
  * Return: (0)
  */
 
-int main()
-(
-char x[] = {'_','p','u','t','c','h','a','r','\n'}
-int _putchar(char x); 
-return (0);
-)
+int _putchar(char x[])
+{
+x[]={'_','p','u','t','c','h','a','r','\n'};
+return (write(1,%x,1));
+}
