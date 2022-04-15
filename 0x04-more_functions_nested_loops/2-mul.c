dc8bfd7121849551bde035d4b9d0fde5082833b8
preyedebo1996 <preyedebo@gmail.com>
2022-04-15 02:47:17 -0700

    fix2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,15 +2,15 @@
 
 /**
  * mul - multiplies two int
- * @a print int
- * @b print int
+ *@a print int
+ *@b print int
  * Return: always 0 on success
  */
 
 int mul(int a, int b)
 {
-int product;
-product = a * b;
+int p;
+p = a * b;
 
-return product;
+return p;
 }
