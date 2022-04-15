380245ae41d9fdef19c495785367d668b88f11a2
preyedebo1996 <preyedebo@gmail.com>
2022-04-15 09:05:11 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numvers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers
+ * Return: always 0 on success
+ */
+
+void print_numbers(void)
+{
+int i= 0;
+while (i < 10)
+{
+_putchar (i);
+i++
+}
+_putchar ('\n');
+}
