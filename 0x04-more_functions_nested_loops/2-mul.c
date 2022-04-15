b94ee1ef3b1bd3b1b032ca4b8113aee2ccdd8b97
preyedebo1996 <preyedebo@gmail.com>
2022-04-15 02:43:21 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two int
+ * @a print int,@b print int
+ * Return: always 0 on success
+ */
+
+int mul(int a, int b)
+{
+int product;
+product = a * b;
+
+return product;
+}
