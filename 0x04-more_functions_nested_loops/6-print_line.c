f7cf7d3ada3701acaf6f0d370efa5da74409e810
preyedebo1996 <preyedebo@gmail.com>
2022-04-15 11:25:38 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,22 @@
-#includei "main.h"
+#include "main.h"
+
 /**
- * print_line - draws a straight line in the terminal.
- * Return: Always 0.
+ * print_line - prints underscores
+ *@n: limit
+ *
+ * Return: void
  */
+
 void print_line(int n)
 {
-
 int i;
 
 if (n > 0)
 {
-for (i = 0; i <= n; i++)
+for (i = 0; i < n; i++)
 {
-_putchar (95);
+_putchar('_');
 }
-
-_putchar ('\n');
 }
-else
-{
-_putchar ('\n');
-}
-
+_putchar('\n');
 }
