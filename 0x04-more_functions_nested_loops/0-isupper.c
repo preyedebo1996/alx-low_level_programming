b67dca4f06a43d7b3f2f98ae1e4ca0581d4859e8
preyedebo1996 <preyedebo@gmail.com>
2022-04-15 01:57:34 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper : starting point
+ * Return (1) if c is uppercase, (0) if c is not
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}else
+{
+return (0);
+}
+}
