d8d9bbb17aa57cee85a6d0e6368b42958dd058b3
preyedebo1996 <preyedebo@gmail.com>
2022-04-20 15:37:49 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: pointer variable of type char
+ * Return: always 0.
+ */
+
+void _puts(char *str)
+{
+	_putchar (*str);
+}
