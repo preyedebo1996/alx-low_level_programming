e7171b4a99bca826249680832ad26c8d1cf16c42
preyedebo1996 <preyedebo@gmail.com>
2022-04-20 16:41:43 -0700

    fix8

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,13 @@
 void print_rev(char *s)
 {
 	int u = 0;
-	char *v;
 
-	v = s;
+	while (u != '\n')
+	{
+	u++;
+	}
 
-	for (u = '\n'; v[u]; u--)
+	for (u = u - 1; u >= 0; u--)
 	{
 	_putchar (v[u]);
 	}
