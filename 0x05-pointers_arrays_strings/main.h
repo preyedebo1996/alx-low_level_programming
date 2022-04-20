b8c2313c5255479617e5585a06b1456c48364a2e
preyedebo1996 <preyedebo@gmail.com>
2022-04-20 13:51:10 -0700

    Task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
 int _putchar(char c);
-void reset-to_98(int *n);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
