721f6587f36bc99b287ab0de3c345d0ba0235387
preyedebo1996 <preyedebo@gmail.com>
2022-04-21 10:56:46 -0700

    fix1

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,12 @@
 void puts2 (char *str)
 {
 	int i = 0;
-	char s = *str;
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; str[i]; i++)
 	{
 	if ((i % 2) != 1)
 	{
-	_putchar (s[i]);
+	_putchar (str[i]);
 	}
 	_putchar ('\n');
 }
