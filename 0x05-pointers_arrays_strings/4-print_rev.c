b4b63c98665a3387e9e586d620b850dab4d96841
preyedebo1996 <preyedebo@gmail.com>
2022-04-20 22:40:08 -0700

    fix3

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,22 +2,42 @@
 /**
  * print_rev - prints a string, in reverse
  * followed by a new line
+ * _strlen - prints length of a string
  * @s: pointer variable with type char
  * Return: always 0.
  */
+int _strlen(char *s);
 
 void print_rev(char *s)
 {
-	int u = 0;
+	int u = 0, v, len;
+	int endidx, startidx;
 
-	while (u != '\0')
+	len = _strlen(s);
+	endidx = len -1;
+
+	for (u = len - 1; u >= 0; u--)
 	{
-	u++;
+	
+	if (s[u] == ' ' || u == 0)
+	{
+	if (u == 0)
+	{
+	startidx = 0;
+	}
+	else
+	{
+	startidx = u + 1;
 	}
 
-	for (u = (u - 1); u >= 0; u--)
+	for (v = startidx; v <= endidx; v++)
 	{
-	_putchar (s[u]);
+	_putchar (s[v]);
+	}
+	endidx = u - 1;
+	_putchar (" ");
+	}
+
 	}
 
 	_putchar ('\n');
