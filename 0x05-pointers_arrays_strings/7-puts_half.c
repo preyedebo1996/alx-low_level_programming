886672046934b9f88098c59a5ca2473bda55a48f
preyedebo1996 <preyedebo@gmail.com>
2022-04-21 11:29:12 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string followed by a new line
+ * if the number of characters is odd, it prints the last n characters
+ * @str: pointer variable of type char
+ *
+ * Return: always 0 on success, 1 on error.
+ */
+void puts_half(char *str)
+{
+	int i, j, lidx, n;
+
+	i = 0;
+
+	for (i = 0; str[i]; i++)
+	{
+	lidx = i;
+	}
+	n = (lidx - 1) / 2;
+
+	if ((lidx % 2) != 0)
+	{
+	for (j = n; str[j]; j++)
+	{
+	_putchar (str[j]);
+	}
+	}
+
+	_putchar ('\n');
+}
