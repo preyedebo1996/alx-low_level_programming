9944dd26f44187af94c72878e3a7c6a2fa6dc614
preyedebo1996 <preyedebo@gmail.com>
2022-04-21 00:01:37 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* rev_string - reverses a string
+*@s: pointer variable of type char
+* Return: 0 on success, 1 on error.
+*/
+void rev_string(char *s)
+{
+	int len, startidx, i = 0;
+
+	while (s[i] != '\n')
+	{
+	i++;
+	}
+	len = i;
+
+	for (startidx = (len - 1); startidx >= 0; startidx--)
+	{
+	_putchar (s[startidx]);
+	}
+}
