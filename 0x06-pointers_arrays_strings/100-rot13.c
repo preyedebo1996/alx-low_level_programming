77a0e56c4df117f40a5849d0c037068a544539ad
preyedebo1996 <preyedebo@gmail.com>
2022-04-23 08:03:55 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * rot13 - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] > 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i] <= 'Z'))
+			{
+				s[i] -= 13;
+				break;
+			}
+
+			s[i] += 13;
+			break;
+		}
+
+		i++;
+	}
+
+	return (s);
+}
