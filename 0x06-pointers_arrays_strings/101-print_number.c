17afeb7951dd74f1a6f1c5c44687e27f514a5e9c
preyedebo1996 <preyedebo@gmail.com>
2022-04-23 08:28:26 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ *
+ * Return: always 0 on success, 1 on error.
+ */
+void print_number(int n)
+{
+	_putchar (n);
+}
