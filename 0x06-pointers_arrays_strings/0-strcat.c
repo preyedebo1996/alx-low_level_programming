88a95ade3b26d7c9b23fd2a76d59a934a0f18dd2
preyedebo1996 <preyedebo@gmail.com>
2022-04-23 06:07:02 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int count = 0, lsrc, ldest, i, j;
+	int count = 0, lsrc, ldest, i, j;
 
-    for (count = 0; count <= src[count] && (src[count] != '\0'); count++)
-    {
-    lsrc = count + 1;
-    }
+	for (count = 0; count <= src[count] && (src[count] != '\0'); count++)
+	{
+	lsrc = count + 1;
+	}
 
-    for (i = 0; i <= dest[i] && (dest[i] != '\0'); dest++)
-    {
-    ldest = i + 1;
-    }
+	for (i = 0; i <= dest[i] && (dest[i] != '\0'); dest++)
+	{
+	ldest = i + 1;
+	}
 
-    for (j = 0; j <= lsrc; j++)
-    {
-    dest[ldest + 1] = src[j];
-    }
+	for (j = 0; j <= lsrc; j++)
+	{
+	dest[ldest + 1] = src[j];
+	}
 
-    return (dest);
+	return (dest);
 }
