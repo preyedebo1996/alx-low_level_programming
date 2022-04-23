e77f9fdc8bf967b732dd8585f70122a6ec4d3aeb
preyedebo1996 <preyedebo@gmail.com>
2022-04-23 06:58:08 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,16 +7,18 @@
 *@n: integer variable indicating number of characters
 * Return: always 0 on success, 1 on error.
 */
+
 char *_strncat(char *dest, char *src, int n)
 {
-int count = 0, ldest, i, j;
+int count = 0, ldest = 0, i = 0, j = 0;
 
 for (i = 0; i <= dest[i] && (dest[i] != '\0'); dest++)
 {
 ldest = i + 1;
 }
+ldest = ldest + 1;
 
-for (j = 0; j < n; j++)
+for (j = 0; j <= n; j++)
 {
 dest[ldest + 1] = src[j];
 }
