695c423c262a595611f87b3fb133632bae45626b
preyedebo1996 <preyedebo@gmail.com>
2022-04-23 05:57:55 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings, overwriting
+ * the terminating null byte (\0) at the end of dest
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * 
+ * Return: always 0 on success, 1 on error.
+ */
+char *_strcat(char *dest, char *src)
+{
+    int count = 0, lsrc, ldest, i, j;
+
+    for (count = 0; count <= src[count] && (src[count] != '\0'); count++)
+    {
+    lsrc = count + 1;
+    }
+
+    for (i = 0; i <= dest[i] && (dest[i] != '\0'); dest++)
+    {
+    ldest = i + 1;
+    }
+
+    for (j = 0; j <= lsrc; j++)
+    {
+    dest[ldest + 1] = src[j];
+    }
+
+    return (dest);
+}
