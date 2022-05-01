9578c1b517d3839ea47901af2d4cb404f7c2b813
preyedebo1996 <preyedebo@gmail.com>
2022-05-01 11:19:43 -0700

    fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,6 +15,5 @@ int _pow_recursion(int x, int y)
                 return (1);
 
         if ( y > 0)
-		return ( x = x * (_pow_recursion( x, (y--))));
-	return x;
+		return (x * (_pow_recursion( x, (y - 1))));
 }
