350454873566bed7e8202a292166df1eff022d51
preyedebo1996 <preyedebo@gmail.com>
2022-05-13 00:25:44 -0700

    fix4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,21 +4,21 @@
  * main - multiplies two numbers and prints the result
  * @argc: number of arguments
  * @argv: pointer to string of arguments
- * @a: first argument passed
- * @b: second argument passed
  * Return: 0 on success, and 1 on error.
  */
 
-int main(int argc, char *argv[int a ,int b])
+int main(int argc, char *argv[])
 {
-	int product;
+	int product, a, b;
 
-	if (argc > 2)
+	if (argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
+	a = argv[1];
+	b = argv[2];
 	product = (a * b);
 
 	printf("%d\n", product);
