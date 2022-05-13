5a3050a68fa96f3af6d3bab1810d8815220ad11d
preyedebo1996 <preyedebo@gmail.com>
2022-05-13 00:45:41 -0700

    fix5

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - multiplies two numbers and prints the result
  * @argc: number of arguments
  * @argv: pointer to string of arguments
  * Return: 0 on success, and 1 on error.
  */
-
 int main(int argc, char *argv[])
 {
 	int product, a, b;
 
-	if (argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
