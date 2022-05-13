bd86699d2dc12d2f83cec5cea0cb914e4b9884cc
preyedebo1996 <preyedebo@gmail.com>
2022-05-13 01:22:19 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - ads two numbers
+ * @argv: pointer to argument array
+ * @argc: argument count
+ * Return: 0 on success, 1 on error.
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned int num, digit, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
