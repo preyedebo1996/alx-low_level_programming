80e1fc64019fabe00bae7dd5d5c45ad2c1609f01
preyedebo1996 <preyedebo@gmail.com>
2022-05-12 23:14:40 -0700

    task2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the number of arguments passed to it
+*@argc: number of arguments
+*@argv: poinnter to string of arguments
+* Return: 0 on success.
+*/
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
