bedd0dc76653ac375f3830fa8ab964eaf2ee9548
preyedebo1996 <preyedebo@gmail.com>
2022-05-15 08:10:44 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,13 +24,13 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i]; i++)
 	{
-		count1++
+		count1++;
 	}
 	count1 += 1;
 
 	for (i = 0; s2[i]; i++)
 	{
-		count2++
+		count2++;
 	}
 	count2 += 1;
 
