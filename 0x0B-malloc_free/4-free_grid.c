652113307d4b956bbc6c14f2215d9294f045b5ac
preyedebo1996 <preyedebo@gmail.com>
2022-05-17 04:37:48 -0700

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,10 +10,11 @@ void free_grid(int **grid, int height)
 {
 	int index;
 
+	if (grid == NULL || height <= 0)
+	return;
+
 	for (index = 0; index < height; index++)
 		free(grid[index]);
 
-	/**
-	 * free(grid);
-	 */
+	free(grid);
 }
