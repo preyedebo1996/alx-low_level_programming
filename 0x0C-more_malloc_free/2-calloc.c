bd4585dacfab981a873d6fad0685ac1a2ef1a892
preyedebo1996 <preyedebo@gmail.com>
2022-05-22 01:56:24 -0700

    Task 5

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,32 +1,39 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
-* _calloc - allocates memory for an array, using malloc.
-* @nmemb: number of elements
-* @size: size of each element
-* Return: pointer to concatenated string
-*/
-void *_calloc(unsigned int nmemb, unsigned int size)
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int *ary[];
 	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
 	if (nmemb == 0 || size == 0)
-	return (NULL);
-
-
-	ary = malloc(nmemb * size);
-	if (ary == NULL)
-	{
-		free(ary);
 		return (NULL);
-	}
+	p = malloc(nmemb * size);
 
-	for (i = 0; i < (nmemb * size); i++)
-	{
-		ary[i] = 0;
-	}
-
-	return (ary);
+	if (p == NULL)
+		return (NULL);
+	_memset(p, 0, (nmemb * size));
+	return (p);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,32 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
-* array_range - creates an array of integers.
-* @min: minimum number of elements
-* @max: maximum number of elements
-* Return: pointer to the newly created array.
-*/
+ * array_range - creates an array of integers
+ * @min: minimum
+ * @max: maximum
+ * Return: array
+ */
 int *array_range(int min, int max)
 {
-	int *arr[];
-	int i;
+	int *arr, m = min, i = 0;
 
 	if (min > max)
-	return (NULL);
+		return (0);
+	arr = malloc((max - min + 1) * sizeof(int));
 
-	arr = malloc(max * sizeof(*arr));
-	if (arr == NULL)
-	{
-		free(arr);
-		return (NULL);
-	}
-
-	for (i = min; i <= max; i++)
-	{
-		arr[i];
-	}
-
+	if (!arr)
+		return (0);
+	while (i <= max - min)
+		arr[i++] = m++;
 	return (arr);
-	free(arr);
 }
